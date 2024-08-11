1652fe8a43952044c79fea4737caefc14807e591
Eric <eric.strohmaier00@gmail.com>
2024-08-11 22:07:27 +0200

    log corn

--- express/cronJobs.js
+++ express/cronJobs.js
@@ -4,13 +4,14 @@ const cron = require("node-cron");
 
 module.exports = {
   startCronJobs: () => {
+    console.log("Starting cron jobs");
     // Define your cron jobs here
     cron.schedule("*/5 * * * *", async () => {
       console.log("Running a cron job every 5 minutes");
       const res = await fetch(
         "https://slacktivity.app/api/slack/schleude?secret=secret_token_007_007"
       );
-      console.log("calling slacktivity", res);
+      console.log("calling slacktivity", res.status);
     });
   },
 };
