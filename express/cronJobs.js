f6a0faa3ab9d6c8f8a1718a9b236ef228a2bd557
Eric <eric.strohmaier00@gmail.com>
2024-08-11 22:24:30 +0200

    update corne?

--- express/cronJobs.js
+++ express/cronJobs.js
@@ -7,11 +7,15 @@ module.exports = {
     console.log("Starting cron jobs");
     // Define your cron jobs here
     cron.schedule("*/5 * * * *", async () => {
-      console.log("Running a cron job every 5 minutes");
-      const res = await fetch(
-        "https://slacktivity.app/api/slack/schleude?secret=secret_token_007_007"
-      );
-      console.log("calling slacktivity", res.status);
+      try {
+        console.log("Running a cron job every 5 minutes");
+        const res = await fetch(
+          "https://slacktivity.app/api/slack/schleude?secret=secret_token_007_007"
+        );
+        console.log("Calling slacktivity", res.status);
+      } catch (error) {
+        console.error("Error in cron job:", error);
+      }
     });
   },
 };
