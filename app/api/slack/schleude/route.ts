51949b32a990d93accfe2e910154d66735724044
Eric <eric.strohmaier00@gmail.com>
2024-09-30 12:03:52 +0200

    remove try

--- app/api/slack/schleude/route.ts
+++ app/api/slack/schleude/route.ts
@@ -15,6 +15,7 @@ export async function GET(request: NextRequest) {
   }
 
   const supabase = supabaseAdmin();
+
   const { data: workspaces, error } = await supabase
     .from("workspace")
     .select("*")
@@ -37,93 +38,22 @@ export async function GET(request: NextRequest) {
   console.log("workspaces", workspaces);
   const updateResults = await Promise.all(
     workspaces.map(async (workspace) => {
-      try {
-        const {
-          slack_auth_token: token,
-          working_hours: workHours,
-          stripe_is_paid: isPaid,
-        } = workspace as Workspace;
-        console.log("workspace as Workspace", workspace);
+      const {
+        slack_auth_token: token,
+        working_hours: workHours,
+        stripe_is_paid: isPaid,
+      } = workspace as Workspace;
+      console.log("workspace as Workspace", workspace);
 
-        // Log activity for unpaid workspaces
-        if (!isPaid) {
-          console.log(`Skipping workspace ${workspace.id}: not paid`);
-          const { error: activityError } = await supabase
-            .from("activity_logs")
-            .insert({
-              workspace_id: workspace.id,
-              action: "not_paid",
-              details: { message: "Workspace is not paid" },
-            });
-
-          if (activityError) {
-            console.error(
-              `Error inserting activity log for workspace ${workspace.id}:`,
-              activityError
-            );
-            return {
-              id: workspace.id,
-              status: "error",
-              message: activityError,
-            };
-          }
-
-          return { id: workspace.id, status: "not_paid" };
-        }
-
-        // Skip if token or work hours are missing
-        if (!token || !workHours) {
-          console.error(
-            `Missing token or work hours for workspace ${workspace.id}`
-          );
-          return { id: workspace.id, status: "missing_data" };
-        }
-
-        const slack = new WebClient(token);
-
-        // Get the current time in the workspace's timezone
-        const now = new Date();
-        const workspaceLocalTime = new Date(
-          formatInTimeZone(now, workHours.timezone, "yyyy-MM-dd'T'HH:mm:ssXXX")
-        );
-
-        const currentDay = workspaceLocalTime.getDay();
-        const currentHour = workspaceLocalTime.getHours();
-        const currentMinute = workspaceLocalTime.getMinutes();
-
-        console.log(
-          `Workspace ${
-            workspace.id
-          } local time: ${workspaceLocalTime.toISOString()}`
-        );
-        console.log(
-          `Current day: ${currentDay}, Current hour: ${currentHour}, Current minute: ${currentMinute}`
-        );
-
-        let action: string;
-
-        if (
-          workHours.daysOfWeek.includes(currentDay) &&
-          (currentHour > workHours.startHour ||
-            (currentHour === workHours.startHour && currentMinute >= 0)) &&
-          (currentHour < workHours.endHour ||
-            (currentHour === workHours.endHour && currentMinute === 0))
-        ) {
-          await slack.users.setPresence({ presence: "auto" });
-          action = "set_active";
-          console.log(`Setting workspace ${workspace.id} to active`);
-        } else {
-          await slack.users.setPresence({ presence: "away" });
-          action = "set_away";
-          console.log(`Setting workspace ${workspace.id} to away`);
-        }
-
-        const { data: activityLog, error: activityError } = await supabase
+      // Log activity for unpaid workspaces
+      if (!isPaid) {
+        console.log(`Skipping workspace ${workspace.id}: not paid`);
+        const { error: activityError } = await supabase
           .from("activity_logs")
           .insert({
             workspace_id: workspace.id,
-            action,
-            details: { currentDay, currentHour, currentMinute, workHours },
+            action: "not_paid",
+            details: { message: "Workspace is not paid" },
           });
 
         if (activityError) {
@@ -131,17 +61,80 @@ export async function GET(request: NextRequest) {
             `Error inserting activity log for workspace ${workspace.id}:`,
             activityError
           );
-          return { id: workspace.id, status: "error", message: activityError };
+          return {
+            id: workspace.id,
+            status: "error",
+            message: activityError,
+          };
         }
 
-        return { id: workspace.id, status: "success" };
-      } catch (error) {
-        console.error(
-          `Error updating presence for workspace ${workspace.id}:`,
-          error
-        );
-        return { id: workspace.id, status: "error", message: error };
+        return { id: workspace.id, status: "not_paid" };
       }
+
+      // Skip if token or work hours are missing
+      if (!token || !workHours) {
+        console.error(
+          `Missing token or work hours for workspace ${workspace.id}`
+        );
+        return { id: workspace.id, status: "missing_data" };
+      }
+
+      const slack = new WebClient(token);
+
+      // Get the current time in the workspace's timezone
+      const now = new Date();
+      const workspaceLocalTime = new Date(
+        formatInTimeZone(now, workHours.timezone, "yyyy-MM-dd'T'HH:mm:ssXXX")
+      );
+
+      const currentDay = workspaceLocalTime.getDay();
+      const currentHour = workspaceLocalTime.getHours();
+      const currentMinute = workspaceLocalTime.getMinutes();
+
+      console.log(
+        `Workspace ${
+          workspace.id
+        } local time: ${workspaceLocalTime.toISOString()}`
+      );
+      console.log(
+        `Current day: ${currentDay}, Current hour: ${currentHour}, Current minute: ${currentMinute}`
+      );
+
+      let action: string;
+
+      if (
+        workHours.daysOfWeek.includes(currentDay) &&
+        (currentHour > workHours.startHour ||
+          (currentHour === workHours.startHour && currentMinute >= 0)) &&
+        (currentHour < workHours.endHour ||
+          (currentHour === workHours.endHour && currentMinute === 0))
+      ) {
+        await slack.users.setPresence({ presence: "auto" });
+        action = "set_active";
+        console.log(`Setting workspace ${workspace.id} to active`);
+      } else {
+        await slack.users.setPresence({ presence: "away" });
+        action = "set_away";
+        console.log(`Setting workspace ${workspace.id} to away`);
+      }
+
+      const { data: activityLog, error: activityError } = await supabase
+        .from("activity_logs")
+        .insert({
+          workspace_id: workspace.id,
+          action,
+          details: { currentDay, currentHour, currentMinute, workHours },
+        });
+
+      if (activityError) {
+        console.error(
+          `Error inserting activity log for workspace ${workspace.id}:`,
+          activityError
+        );
+        return { id: workspace.id, status: "error", message: activityError };
+      }
+
+      return { id: workspace.id, status: "success" };
     })
   );
 
