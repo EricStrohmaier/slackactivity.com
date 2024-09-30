4002890a866f544d525070096ac7e09614ae32aa
Eric <eric.strohmaier00@gmail.com>
2024-09-30 17:01:32 +0200

    update @supabase/supabase-js

--- app/api/slack/schleude/route.ts
+++ app/api/slack/schleude/route.ts
@@ -1,7 +1,8 @@
 import { NextRequest } from "next/server";
-import { supabaseAdmin } from "@/utils/supabase/admin";
+
 import { WebClient } from "@slack/web-api";
 import { Workspace } from "@/types/supabase";
+import { supabaseAdmin } from "@/utils/supabase/admin";
 
 export async function GET(request: NextRequest) {
   const secretToken = request.nextUrl.searchParams.get("secret");
@@ -15,10 +16,12 @@ export async function GET(request: NextRequest) {
 
   const supabase = supabaseAdmin();
 
+  // Ensure no caching for up-to-date data
   const { data: workspaces, error } = await supabase
     .from("workspace")
     .select("*")
     .eq("is_active", true);
+
   if (error) {
     console.error("Error fetching active workspaces:", error);
     return new Response(
@@ -51,6 +54,7 @@ export async function GET(request: NextRequest) {
         working_hours: workHours,
         stripe_is_paid: isPaid,
       } = workspace as Workspace;
+
       console.log("workspace as Workspace", workspace);
 
       // Log activity for unpaid workspaces
--- utils/supabase/admin.ts
+++ utils/supabase/admin.ts
@@ -3,9 +3,18 @@
 import { Database } from "@/types/supabase_generated";
 import { createClient } from "@supabase/supabase-js";
 
+const supabase_url = process.env.NEXT_PUBLIC_SUPABASE_URL || "";
+const service_role_key = process.env.SUPABASE_SERVICE_ROLE_KEY || "";
 // as it has admin privileges and overwrites RLS policies!
 export const supabaseAdmin = () =>
-  createClient<Database>(
-    process.env.NEXT_PUBLIC_SUPABASE_URL || "",
-    process.env.SUPABASE_SERVICE_ROLE_KEY || ""
-  );
+  createClient<Database>(supabase_url, service_role_key);
+
+const supabase = createClient(supabase_url, service_role_key, {
+  auth: {
+    autoRefreshToken: false,
+    persistSession: false,
+  },
+});
+
+// Access auth admin api
+const adminAuthClient = supabase.auth.admin;
